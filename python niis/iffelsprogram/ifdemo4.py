6378eef71e29d23cbed4dadfb775aa6401ada119
Saninee priyadarshini <sanineepriyadarshini046@gmail.com>
2026-02-23 12:32:36 +0530

    if else program

---
+++ python niis/iffelsprogram/ifdemo4.py
@@ -0,0 +1,7 @@
+
+"""wap check no is 0dd no"""
+print("enter a number")
+no=int(input())
+if no%2!=0:
+	 print("odd no")
+	
---
+++ python niis/iffelsprogram/ifdemo6.py
@@ -0,0 +1,8 @@
+"""wap take a number from keybord check no is +ve,-ve and Zero"""
+print("enter a number")
+if n>0:
+   print("+ve")
+if n<0:
+   print("-ve")
+if n==0:
+   print("zero")
