b794cbba64c1bfde71e547c33ee4bddb898a4b4e
Saninee priyadarshini <sanineepriyadarshini046@gmail.com>
2026-04-29 06:49:40 +0530

    program

---
+++ python niis/DSAusing python/D.py
@@ -0,0 +1,72 @@
+
+class Node:
+    def _init_(self, ele):
+        self.prev=None #new add
+        self.data = ele
+        self.next = None
+class DLinkedList:
+    def _init_(self):
+        self.head = None
+    def create(self):
+        ch = True
+        c = 0
+        ptr = None
+        while ch:
+            c = c + 1
+            print("Enter node", c, "data")
+            ele = int(input())
+            cur = Node(ele)
+            if self.head == None:
+                self.head = cur
+            else:
+                ptr.next = cur
+                cur.prev=ptr #new add
+            ptr = cur
+
+            print("Do you continue? Press True/False")
+            ch = input()
+
+            if ch == "True":
+                ch = True
+            else:
+                ch = False
+
+    def displayf(self):
+        if self.head==None:
+            print("no element ")
+            return 
+        print("Elements are forward")
+        ptr = self.head
+
+        while ptr != None:
+            print(ptr.data)
+            ptr = ptr.next
+    def displayb(self):
+        if self.head==None:
+            print("no element ")
+            return
+        print("elements in backward")
+        ptr=self.head
+        while ptr.next!=None:
+            ptr=ptr.next
+        while ptr!=None:
+            print(ptr.data)
+            ptr=ptr.prev
+    def insertbeg(self):
+        if self.head==None:
+            print("no element")
+            return
+        print("delete elements=",self.head.data)
+        self.head=self.head.next
+        self.head.prev=None
+
+       
+
+
+obj =DLinkedList()
+obj.create()
+obj.displayf()
+obj.displayb()
+obj.deletebeg(5)
+obj.displayf()
+obj.displayb()
---
+++ python niis/DSAusing python/delete at end.py
@@ -0,0 +1,95 @@
+class Node:
+    def __init__(self, ele):
+        self.data = ele
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def create(self):
+        ch = True
+        c = 0
+        ptr = None
+
+        while ch:
+            c = c + 1
+            print("Enter node", c, "data")
+            ele = int(input())
+
+            cur = Node(ele)
+
+            if self.head == None:
+                self.head = cur
+            else:
+                ptr.next = cur
+
+            ptr = cur
+
+            print("Do you continue? Press True/False")
+            ch = input()
+
+            if ch == "True":
+                ch = True
+            else:
+                ch = False
+
+    def display(self):
+        print("Elements are")
+        ptr = self.head
+
+        while ptr != None:
+            print(ptr.data)
+            ptr = ptr.next
+
+    def insertend(self, data):
+        cur = Node(data)
+        print("data insert end")
+        if self.head == None:
+            self.head = cur
+            return
+        ptr = self.head
+        while ptr.next != None:
+            ptr = ptr.next
+        ptr.next = cur
+
+    def deletebeg(self):
+        if self.head is None:
+            print("List is empty, nothing to delete")
+            return
+        
+        print("Deleted element:", self.head.data)
+        self.head = self.head.next
+
+    def deleteend(self):
+        if self.head is None:
+            print("List is empty, nothing to delete")
+            return
+
+        if self.head.next is None:
+            print("Deleted element:", self.head.data)
+            self.head = None
+            return
+
+        ptr = self.head
+        while ptr.next.next != None:
+            ptr = ptr.next
+
+        print("Deleted element:", ptr.next.data)
+        ptr.next = None
+
+
+# Driver code
+obj = LinkedList()
+obj.create()
+obj.display()
+
+obj.insertend(5)
+obj.display()
+
+obj.deletebeg()
+obj.display()
+
+obj.deleteend()
+obj.display()
